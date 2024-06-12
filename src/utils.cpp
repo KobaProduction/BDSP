e5e84faffc6b410eb3e6b9e47d0aead5366c3e1c
Arthur Koba <caprimores@gmail.com>
2024-06-12 03:26:29 +0300

    check arduino framework

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,8 @@
 #include "./utils.h"
 
 void print_status(const char str[], void *status) {
+#ifdef ARDUINO
     Serial.print(str);
     Serial.println(*reinterpret_cast<uint8_t*>(status));
+#endif
 }
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,9 @@
 #ifndef BDSP_UTILS_H
 #define BDSP_UTILS_H
 
+#ifdef ARDUINO
 #include <Arduino.h>
+#endif
 
 void print_status(const char str[], void *status);
 
