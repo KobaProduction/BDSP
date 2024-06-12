7b4ee6547d4f443470ccf8a32f07e64fbcbd4a31
Arthur Koba <caprimores@gmail.com>
2024-06-12 03:17:35 +0300

    release COBSReader

--- src/encoders/cobs/reader.cpp
+++ src/encoders/cobs/reader.cpp
@@ -1,13 +1,75 @@
 #include "./reader.h"
 
-COBSReader::COBSReader(cobs_config_t config) {
-
-}
-
-COBSReader::~COBSReader() {
-
+COBSReader::COBSReader(cobs_config_t config_, callback_t callback_) {
+    config = config_;
+    callback = callback_;
+    reset();
 }
 
 void COBSReader::reset() {
+    state = SERVICE_BYTE;
+    swap_byte_offset = 0;
+    service_byte_offset = config.depth;
+}
 
+void COBSReader::parse(uint8_t character) {
+    switch (state) {
+        case SERVICE_BYTE:
+            if (character == config.delimiter) return reset();
+            set_swap_byte_offset(character);
+            break;
+        case REGULAR_BYTE:
+            if (character == config.delimiter) {
+                // Decoding error. The separator should not appear in regular data.
+                return set_error_state(character);
+            }
+            callback(character, READ_OK);
+            break;
+        case SWAP_BYTE:
+            if (character == config.delimiter) {
+                callback(character, READ_END);
+                return reset();
+            }
+            set_swap_byte_offset(character);
+            callback(character, READ_OK);
+            break;
+        case WAIT_SEPARATOR:
+            if (character == config.delimiter) reset();
+            break;
+    }
+    if (state == WAIT_SEPARATOR) return;
+    service_byte_offset--;
+    swap_byte_offset--;
+    if (service_byte_offset == 0) {
+        state = SERVICE_BYTE;
+        service_byte_offset = config.depth;
+    } else if (swap_byte_offset == 0){
+        state = SWAP_BYTE;
+    } else {
+        state = REGULAR_BYTE;
+    }
+}
+
+void COBSReader::parse(uint8_t *characters, size_t size) {
+    for (int i = 0; i < size; ++i) {
+        parse(characters[i]);
+    }
+}
+
+void COBSReader::set_swap_byte_offset(uint8_t offset) {
+    if (offset > config.depth) {
+        // Decoding error. The size of the new swap_byte_offset cannot exceed the swap_byte_offset from the configuration.
+        set_error_state(offset);
+    }
+    // Substitution of swap_byte_offset if the delimiter in the configuration is not equal to 0.
+    if (not offset and config.delimiter) {
+        swap_byte_offset = config.delimiter;
+    } else {
+        swap_byte_offset = offset;
+    }
+}
+
+void COBSReader::set_error_state(uint8_t character = 0) {
+    callback(character, READ_ERROR);
+    state = WAIT_SEPARATOR;
 }
--- src/encoders/cobs/reader.h
+++ src/encoders/cobs/reader.h
@@ -3,15 +3,25 @@
 
 #include "./config.h"
 
-enum cobs_read_state {PACKED_ID, SIZE_A, SIZE_B, DATA, DELIMITER};
+enum cobs_fsm_state {SERVICE_BYTE, REGULAR_BYTE, SWAP_BYTE, WAIT_SEPARATOR};
+enum cobs_read_state {READ_OK, READ_END, READ_ERROR};
+
+typedef void (*callback_t)(uint8_t character, cobs_read_state read_state);
 
 class COBSReader {
 public:
-    explicit COBSReader(cobs_config_t config);
-    ~COBSReader();
+    explicit COBSReader(cobs_config_t config, callback_t callback);
     void reset();
+    void parse(uint8_t character);
+    void parse(uint8_t *characters, size_t size);
 private:
-    cobs_read_state state = PACKED_ID;
+    cobs_config_t config{};
+    callback_t callback = nullptr;
+    cobs_fsm_state state = SERVICE_BYTE;
+    uint8_t swap_byte_offset = 0;
+    uint8_t service_byte_offset = 0;
+    void set_swap_byte_offset(uint8_t offset);
+    void set_error_state(uint8_t character);
 };
 
 #endif //BDSP_READER_H
