730e36e7c490c515a9db3c39aaed41250083ea65
Arthur Koba <caprimores@gmail.com>
2024-06-13 22:23:27 +0300

    update packet

--- src/packet.cpp
+++ src/packet.cpp
@@ -1,15 +1,18 @@
 #include "./packet.h"
 
-
-Packet::Packet(uint8_t id_, uint16_t size_, bool is_need_buffer) {
+Packet::Packet(uint8_t id_, size_t size_, uint8_t *data_ptr) {
     id = id_;
     size = size_;
-    if (is_need_buffer)
-        data = reinterpret_cast<uint8_t*>(malloc(size_));
-    need_free = is_need_buffer;
+    data = data_ptr;
 }
 
 Packet::~Packet() {
     if (need_free) free(data);
 }
 
+bool Packet::create_buffer() {
+    data = reinterpret_cast<uint8_t*>(malloc(size));
+    if (not data) return false;
+    need_free = true;
+    return true;
+}
--- src/packet.h
+++ src/packet.h
@@ -1,18 +1,26 @@
 #ifndef BDSP_PACKET_H
 #define BDSP_PACKET_H
 
-#include <stdint.h>
-#include <stdlib.h>
+#ifdef ARDUINO
+#include "stdint.h"
+#include "stddef.h"
+#include "stdlib.h"
+#else
+#include <cstdint>
+#include <cstddef>
+#include <cstdlib>
+#endif
 
 class __attribute__((packed)) Packet {
 public:
-    Packet(uint8_t id, uint16_t size, bool is_need_buffer = false);
+    Packet(uint8_t id, size_t size, uint8_t *data_ptr = nullptr);
     ~Packet();
-    uint8_t id;
-    uint16_t size;
-    uint8_t *data;
+    bool create_buffer();
+    uint8_t id = 0;
+    uint16_t size = 0;
+    uint8_t *data = nullptr;
 private:
-    bool need_free;
+    bool need_free = false;
 };
 
 #endif //BDSP_PACKET_H
