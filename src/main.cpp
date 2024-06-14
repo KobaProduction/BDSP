d8281a7de7ba7b18fd009858fd197b1f1eaf4e1a
Arthur Koba <caprimores@gmail.com>
2024-06-14 03:16:55 +0300

    update code template

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <Arduino.h>
+#include <bdsp_transceiver.h>
 
-#include <bdsp_transmitter.h>
+#define SIZE_DATA 255
 
-BDSPTransmitter transmitter;
-
-#define COUNT 255
-uint8_t data[COUNT] = {0};
+BDSPTransceiver transceiver;
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
-    cobs_config_t config = {.delimiter = '\n', .depth = 255};
-
-    transmitter.set_config(config, [] (uint8_t *data, size_t size) {Serial.write(data, size);});
+    cobs_config_t config = {.delimiter = '\n', .depth = 30};
+    transceiver.set_config(
+            config,
+            [] (uint8_t *data, size_t size) {Serial.write(data, size); Serial.flush();},
+            [] (Packet &packet, void *context) {
+                Serial.print("Got packet. ID: ");
+                Serial.println(packet.id);
+            },
+            nullptr
+    );
 }
 
 void loop() {
-    transmitter.send_data(1, data, COUNT);
+    uint8_t data[SIZE_DATA] = {0};
+    transceiver.send_data(1, data, SIZE_DATA);
+    size_t available = Serial.available();
+    if (available) {
+        auto *buffer = new uint8_t[available];
+        Serial.readBytes(buffer, available);
+        transceiver.parse(buffer, available);
+        delete [] buffer;
+    }
     delay(5000);
 }
