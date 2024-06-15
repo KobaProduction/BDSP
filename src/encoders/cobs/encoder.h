9d6313a20f41f8e59e504430fc6bc62019a7abd6
Arthur Koba <caprimores@gmail.com>
2024-06-15 04:51:02 +0300

    refactor | indents removed

--- src/encoders/cobs/encoder.h
+++ src/encoders/cobs/encoder.h
@@ -12,8 +12,8 @@ public:
     ~COBSEncoder();
     cobs_encoder_status_t get_status();
     cobs_encoder_status_t finish_sending(bool is_send_with_delimiter = true);
-    void                send_segment(uint8_t *buffer_ptr, size_t size);
-    void                reset();
+    void send_segment(uint8_t *buffer_ptr, size_t size);
+    void reset();
 private:
     cobs_config_t _cfg = {DEFAULT_COBS_DELIMITER, DEFAULT_COBS_DEPTH};
     cobs_write_handler_t _write_handler = nullptr;
