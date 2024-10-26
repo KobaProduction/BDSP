3160586c2b48ab80285e93f27e913c05dbff4cf6
ArthurKoba <caprimores@gmail.com>
2024-10-26 13:26:00 +0300

    add PPP encoder

---
+++ src/encoders/PPP.cpp
@@ -0,0 +1,19 @@
+#include "PPP.h"
+
+using namespace BDSP::encoders::PPP;
+
+void PPPEncoder::_write_delimiter() {
+    _writer(0x7E, _writer_context);
+}
+
+void PPPEncoder::_encode(uint8_t byte) {
+    if (byte == 0x7E) {
+        _writer(0x7D, _writer_context);
+        _writer(0x5E, _writer_context);
+    } else if (byte == 0x7D) {
+        _writer(0x7D, _writer_context);
+        _writer(0x5D, _writer_context);
+    } else {
+        _writer(byte, _writer_context);
+    }
+}
