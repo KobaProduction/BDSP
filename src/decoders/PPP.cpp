f2b878d8c597e47a31e9a8e55ba4ed76031ac835
ArthurKoba <caprimores@gmail.com>
2024-10-26 13:27:00 +0300

    add PPP decoder

---
+++ src/decoders/PPP.cpp
@@ -0,0 +1,31 @@
+#include "PPP.h"
+
+using namespace BDSP::decoders::PPP;
+
+decode_state_t PPPDecoder::_decode(uint8_t byte) {
+    decode_state_t status = DECODE_OK;
+    switch (byte) {
+        case 0x7E:
+            status = DECODE_END;
+            break;
+        case 0x7D:
+            if (is_last_7D) status = DECODE_ERROR;
+            else is_last_7D = true;
+            break;
+        case 0x5D:
+            if (not is_last_7D) break;
+            byte = 0x7D;
+            is_last_7D = false;
+            break;
+        case 0x5E:
+            if (not is_last_7D) break;
+            byte = 0x7E;
+            is_last_7D = false;
+            break;
+        default:
+            break;
+    }
+    if (byte == 0x7D and is_last_7D) return status;
+    _handler(byte, status);
+    return status;
+}
---
+++ src/decoders/PPP.h
@@ -0,0 +1,18 @@
+#ifndef BDSP_DECODERS_PPP_H
+#define BDSP_DECODERS_PPP_H
+
+#include <cstdint>
+#include "./abs_decoder.h"
+
+namespace BDSP::decoders::PPP {
+    using namespace BDSP::decoders;
+
+
+    class PPPDecoder final : public ABS::AbstractDecoder {
+    protected:
+        bool is_last_7D = false;
+
+        decode_state_t _decode(uint8_t byte) override;
+    };
+}
+#endif //BDSP_DECODERS_PPP_H
