93b3e0f08161d237a737483e4ff54ecca9ecb6cd
ArthurKoba <caprimores@gmail.com>
2024-11-12 23:15:19 +0300

    add _is_ready flag, ENCODE_FINISH and ENCODER_ERROR statuses, _writer encapsulation, remove _write_delimiter method

--- include/BDSP/encoders/abs_encoder.h
+++ include/BDSP/encoders/abs_encoder.h
@@ -9,20 +9,25 @@ namespace BDSP::encoders::ABS {
     using namespace BDSP::encoders;
 
     class AbstractEncoder {
-    protected:
         write_handler_t _writer = nullptr;
         void *_writer_context = nullptr;
+    protected:
+        bool _is_ready = true;
+
+        void _write(uint8_t byte);
+
+        void _write(uint8_t *buffer_ptr, size_t size);
 
         virtual void _encode(uint8_t byte) = 0;
-
-        virtual void _write_delimiter() = 0;
 
     public:
         void set_writer(write_handler_t writer, void *context_ptr = nullptr);
 
         encode_status_t encode(uint8_t byte);
 
-        [[maybe_unused]] encode_status_t encode(uint8_t *buffer_ptr, size_t size);
+        encode_status_t encode(uint8_t *buffer_ptr, size_t size);
+
+        virtual encode_status_t finish_encode() = 0;
     };
 }
 
--- src/encoders/abs_encoder.cpp
+++ src/encoders/abs_encoder.cpp
@@ -2,22 +2,31 @@
 
 using namespace BDSP::encoders::ABS;
 
+void AbstractEncoder::_write(uint8_t byte) {
+    _writer(byte, _writer_context);
+}
+
+void AbstractEncoder::_write(uint8_t *buffer_ptr, size_t size) {
+    for (int i = 0; i < size; ++i) _write(buffer_ptr[i]);
+}
+
 void AbstractEncoder::set_writer(write_handler_t writer, void *context_ptr) {
     _writer = writer;
     _writer_context = context_ptr;
 }
 
 encode_status_t AbstractEncoder::encode(uint8_t byte) {
-    if (not _writer) return encode_status_t::WRITER_NOT_INSTALLED_ERROR;
+    if (not _writer) return WRITER_NOT_INSTALLED_ERROR;
+    if (not _is_ready) return ENCODER_ERROR;
     _encode(byte);
-    return encode_status_t::ENCODE_OK;
+    return ENCODE_OK;
 }
 
-[[maybe_unused]] encode_status_t AbstractEncoder::encode(uint8_t *buffer_ptr, size_t size) {
-    encode_status_t status = encode_status_t::ENCODE_OK;
+encode_status_t AbstractEncoder::encode(uint8_t *buffer_ptr, size_t size) {
+    encode_status_t status = ENCODE_OK;
     for (size_t i = 0; i < size; ++i) {
         status = encode(buffer_ptr[i]);
-        if (status not_eq encode_status_t::ENCODE_OK) break;
+        if (status not_eq ENCODE_OK) break;
     }
     return status;
 }
