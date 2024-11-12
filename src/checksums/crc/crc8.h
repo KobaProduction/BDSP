08d4bbadd7340373a9d6734ae924b111f551277d
ArthurKoba <caprimores@gmail.com>
2024-11-12 23:08:19 +0300

    delete unused headers

--- src/checksums/crc/crc8.h
+++
@@ -1,9 +0,0 @@
-#ifndef BDSP_CRC8_H
-#define BDSP_CRC8_H
-
-#include <stdint.h>
-#include <stdlib.h>
-
-uint8_t crc8(const uint8_t *data_ptr, size_t size, uint8_t base = 0xff);
-
-#endif //BDSP_CRC8_H
