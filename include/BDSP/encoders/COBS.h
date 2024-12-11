97ce75847f32a36ef7c17e24399908766557915b
ArthurKoba <caprimores@gmail.com>
2024-12-12 02:13:00 +0300

    adaptation COBS encoder

---
+++ include/BDSP/encoders/COBS.h
@@ -0,0 +1,36 @@
+#ifndef BDSP_DECODERS_PPP_H
+#define BDSP_DECODERS_PPP_H
+
+#include "BDSP/encoders/abs_encoder.h"
+
+#ifndef MIN_BDSP_COBS_DEPTH
+#define MIN_BDSP_COBS_DEPTH 16
+#endif
+
+namespace BDSP::encoders::COBS {
+    using namespace BDSP::encoders;
+
+    struct cobs_config_t {
+        uint8_t delimiter;
+        uint8_t depth;
+    };
+
+    class COBSEncoder final : public ABS::AbstractEncoder {
+        COBS::cobs_config_t _cfg{};
+        uint8_t *_buffer_ptr = nullptr;
+        uint8_t _buffer_position = 1;
+
+        void _encode(uint8_t byte) override;
+
+        void _write_buffer();
+
+    public:
+        explicit COBSEncoder(COBS::cobs_config_t config = {'\0', 255});
+
+        ~COBSEncoder();
+
+        encode_status_t finish_encode() override;
+    };
+}
+
+#endif //BDSP_DECODERS_PPP_H
---
+++ src/encoders/COBS.cpp
@@ -0,0 +1,50 @@
+#include "stdlib.h"
+#include "BDSP/encoders/COBS.h"
+
+using namespace BDSP::encoders::COBS;
+
+COBSEncoder::COBSEncoder(cobs_config_t config) {
+    _cfg = config;
+    if (_cfg.depth < MIN_BDSP_COBS_DEPTH) _cfg.depth = MIN_BDSP_COBS_DEPTH;
+    free(_buffer_ptr);
+    _buffer_ptr = reinterpret_cast<uint8_t *>(malloc(_cfg.depth));
+    if (not _buffer_ptr) _is_ready = false;
+}
+
+void COBSEncoder::_encode(uint8_t byte) {
+    if (_buffer_position == _cfg.depth) {
+        _write_buffer();
+    }
+    if (byte not_eq _cfg.delimiter) {
+        _buffer_ptr[_buffer_position++] = byte;
+    } else {
+        _write_buffer();
+    }
+}
+
+void COBSEncoder::_write_buffer() {
+    _buffer_ptr[0] = _cfg.delimiter ? 0 : _buffer_position;
+    _write(_buffer_ptr, _buffer_position);
+    _buffer_position = 1;
+}
+
+COBSEncoder::~COBSEncoder() {
+    free(_buffer_ptr);
+}
+
+encode_status_t COBSEncoder::finish_encode() {
+    if (not _buffer_ptr) return UNKNOWN_ENCODER_ERROR;
+    if (_buffer_position == _cfg.depth) _write_buffer();
+    _write_buffer();
+    _write(_cfg.delimiter);
+    return ENCODE_FINISH;
+}
+
+
+
+
+
+
+
+
+
