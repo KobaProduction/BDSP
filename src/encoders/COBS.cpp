eed6c58ab7e9552de595471b7b0e5a9656d6f4f7
ArthurKoba <caprimores@gmail.com>
2024-12-15 19:18:00 +0300

    refactor code, use _is_ready

--- src/encoders/COBS.cpp
+++ src/encoders/COBS.cpp
@@ -11,18 +11,14 @@ COBSEncoder::COBSEncoder(cobs_config_t config) {
 }
 
 void COBSEncoder::_encode(uint8_t byte) {
-    if (_buffer_position == _cfg.depth) {
-        _write_buffer();
-    }
-    if (byte not_eq _cfg.delimiter) {
-        _buffer_ptr[_buffer_position++] = byte;
-    } else {
-        _write_buffer();
-    }
+    if (_buffer_position == _cfg.depth) _write_buffer();
+
+    if (byte not_eq _cfg.delimiter) _buffer_ptr[_buffer_position++] = byte;
+    else _write_buffer();
 }
 
 void COBSEncoder::_write_buffer() {
-    _buffer_ptr[0] = _cfg.delimiter ? 0 : _buffer_position;
+    _buffer_ptr[0] = _cfg.delimiter not_eq 0x00 and _buffer_position == _cfg.delimiter ? 0 : _buffer_position;
     _write(_buffer_ptr, _buffer_position);
     _buffer_position = 1;
 }
@@ -32,18 +28,9 @@ COBSEncoder::~COBSEncoder() {
 }
 
 encode_status_t COBSEncoder::finish_encode() {
-    if (not _buffer_ptr) return UNKNOWN_ENCODER_ERROR;
+    if (not _is_ready) return UNKNOWN_ENCODER_ERROR;
     if (_buffer_position == _cfg.depth) _write_buffer();
     _write_buffer();
     _write(_cfg.delimiter);
     return ENCODE_FINISH;
 }
-
-
-
-
-
-
-
-
-
