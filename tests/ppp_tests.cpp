6dc7ba9f15c4155176a8cb809da86e2dfba3125b
ArthurKoba <caprimores@gmail.com>
2024-12-25 12:25:30 +0300

    update ppp tests

--- tests/ppp_tests.cpp
+++ tests/ppp_tests.cpp
@@ -13,7 +13,7 @@ using namespace BDSP::decoders::PPP;
 
 
 TEST(ppp_encoding_test, encoding_test) {
-    BDSP::encoders::PPP::PPPEncoder encoder;
+    PPPEncoder encoder;
 
     std::vector<uint8_t> data = {0x7E, 0x7D, 0x00};
     std::vector<uint8_t> correct_encoded = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
@@ -21,34 +21,62 @@ TEST(ppp_encoding_test, encoding_test) {
     start_test_encoder(encoder, data, correct_encoded, HEX);
 }
 
-TEST(ppp_encoding_test, decoding_test) {
-    BDSP::decoders::PPP::PPPDecoder decoder;
+TEST(ppp_encoding_test, decoding_errors_test) {
+    struct Context {
+        PPPDecoder decoder;
+        std::vector<uint8_t> decoded;
+        std::vector<uint8_t> encoded;
+    } ctx;
 
-    std::vector<uint8_t> data = {0x7E, 0x7D, 0x00};
-    std::vector<uint8_t> encoded = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
+    ctx.decoder.set_data_handler([](uint8_t byte, decode_status_t status, void *ctx) {
+        if (status == DECODE_OK) reinterpret_cast<Context *>(ctx)->decoded.push_back(byte);
+    }, &ctx);
 
-    start_test_decoder(decoder, encoded, data, HEX);
+    for (uint8_t byte = 0; byte < 255; ++byte) {
+        ctx.decoder.reset_decode_state(false);
+        ctx.decoded.clear();
+        ctx.encoded = {0x7D, byte};
+        auto status = ctx.decoder.decode(ctx.encoded.data(), ctx.encoded.size());
+
+        if (byte not_eq 0x5D and byte not_eq 0x5E) {
+            EXPECT_EQ(ctx.decoded.size(), 0);
+        } else {
+            EXPECT_EQ(status, DECODE_OK);
+            EXPECT_EQ(ctx.decoded.size(), 1);
+            EXPECT_EQ(ctx.decoded[0], byte ^ 0x20);
+        }
+    }
+
 }
 
-TEST(ppp_encoding_test, decoding_error_test) {
-    BDSP::decoders::PPP::PPPDecoder decoder;
+TEST(ppp_encoding_test, decoding_normal_test) {
+    struct Context {
+        PPPDecoder decoder;
+        std::vector<uint8_t> encoded;
+        std::vector<uint8_t> decoded;
+        std::vector<uint8_t> correct;
+    } ctx;
 
-    decoder.set_data_handler([](uint8_t byte, decode_status_t status, void *ctx_ptr) {});
+    ctx.encoded = {0x00, 0x01, 0x7E};
+    ctx.correct = {0x00, 0x01};
+    start_test_decoder(ctx.decoder, ctx.encoded, ctx.correct);
 
-    std::vector<uint8_t> correct_encoded = {0x7E};
-    auto status = decoder.decode(correct_encoded[0]);
-    EXPECT_EQ(status, DECODE_END);
+    ctx.decoder.set_data_handler([](uint8_t byte, decode_status_t status, void *ctx) {
+        auto &context = *reinterpret_cast<Context *>(ctx);
+        switch (status) {
+            case DECODE_OK:
+                context.decoded.push_back(byte);
+                break;
+            case DECODE_END:
+                break;
+            case DECODE_ERROR:
+            case UNKNOWN_DECODER_ERROR:
+                FAIL() << "An unforeseen mistake arose";
+        }
+    }, &ctx);
 
-    correct_encoded = {0x7D, 0x7D};
-    status = decoder.decode(correct_encoded.data(), correct_encoded.size());
-    EXPECT_EQ(status, DECODE_ERROR);
-
-    correct_encoded = {0x7D, 0x5D};
-    status = decoder.decode(correct_encoded.data(), correct_encoded.size());
-    EXPECT_EQ(status, DECODE_OK);
-
-    correct_encoded = {0x7D, 0x5E};
-    status = decoder.decode(correct_encoded.data(), correct_encoded.size());
-    EXPECT_EQ(status, DECODE_OK);
-
+    ctx.encoded = {0x7E, 0x7E, 0x00, 0x01, 0x7E};
+    ctx.correct = {0x00, 0x01};
+    ctx.decoder.decode(ctx.encoded.data(), ctx.encoded.size());
+    EXPECT_EQ(is_equals(ctx.decoded, ctx.correct), true);
 }
