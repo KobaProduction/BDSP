dd88c49f3aa5d1a96dc9555fcf65d1a8d871a630
ArthurKoba <caprimores@gmail.com>
2024-12-27 15:23:04 +0300

    fix incorrect checking max_packet_size

--- src/mixins.cpp
+++ src/mixins.cpp
@@ -3,7 +3,7 @@
 using namespace BDSP::core;
 
 set_max_packet_size_status_t MaxPacketSizeMixin::set_max_packet_size(uint16_t max_packet_size) {
-    if (_max_packet_size < 1) return INVALID_MAXIMUM_PACKET_SIZE;
+    if (max_packet_size < 1) return INVALID_MAXIMUM_PACKET_SIZE;
     _max_packet_size = max_packet_size;
     return MAX_PACKET_SIZE_SET;
 }
