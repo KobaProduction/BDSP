06f5dda5adb32ed258fbf05b263803b3277320d7
ArthurKoba <caprimores@gmail.com>
2024-12-30 09:42:32 +0300

    add raw show data

--- examples/Desktop/src/utils.cpp
+++ examples/Desktop/src/utils.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include "utils.h"
 
-void show_data(std::vector<uint8_t> &data, bool use_hex) {
+void show_data(uint8_t *data, size_t size, bool use_hex) {
     std::cout << "Data: ";
-    for (int i = 0; i < data.size(); ++i) {
+    for (int i = 0; i < size; ++i) {
         auto byte = static_cast<unsigned int>(data[i]);
         if (use_hex) {
             if (byte <= 0xF) std::cout << 0;
@@ -13,4 +13,8 @@ void show_data(std::vector<uint8_t> &data, bool use_hex) {
         std::cout << std::dec;
     }
     std::cout << std::endl;
+}
+
+void show_data(std::vector<uint8_t> &data, bool use_hex) {
+    show_data(data.data(), data.size(), use_hex);
 }
