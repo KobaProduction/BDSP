610f1dd23ef3c6e117c5ea91febd9b3058efbbc4
ArthurKoba <caprimores@gmail.com>
2025-01-02 16:40:14 +0300

    update ppp tests

--- tests/ppp_tests.cpp
+++ tests/ppp_tests.cpp
@@ -2,81 +2,79 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-#include <BDSP/encoders/PPP.h>
-#include <BDSP/decoders/PPP.h>
+#include <BDSP/streams/PPP/writer.h>
+#include <BDSP/streams/PPP/reader.h>
 
-#include "utils/show.h"
 #include "utils/testing.h"
 
-using namespace BDSP::encoders::PPP;
-using namespace BDSP::decoders::PPP;
+using namespace BDSP::streams;
 
 
-TEST(ppp_encoding_test, encoding_test) {
-    PPPEncoder encoder;
+TEST(ppp_pipelines_tests, encoding_test) {
+    PPP::PPPWriter writer;
 
     std::vector<uint8_t> data = {0x7E, 0x7D, 0x00};
-    std::vector<uint8_t> correct_encoded = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
+    std::vector<uint8_t> correct = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
 
-    start_test_encoder(encoder, data, correct_encoded, HEX);
+    start_test_writer(writer, data, correct, true);
 }
 
-TEST(ppp_encoding_test, decoding_errors_test) {
+TEST(ppp_pipelines_tests, decoding_errors_test) {
     struct Context {
-        PPPDecoder decoder;
-        std::vector<uint8_t> decoded;
-        std::vector<uint8_t> encoded;
+        PPP::PPPDecoder reader;
+        std::vector<uint8_t> read_buffer;
+        std::vector<uint8_t> write_buffer;
     } ctx;
 
-    ctx.decoder.set_data_handler([](uint8_t byte, decode_status_t status, void *ctx) {
-        if (status == DECODE_OK) reinterpret_cast<Context *>(ctx)->decoded.push_back(byte);
+    ctx.reader.set_data_handler([](uint8_t byte, read_status_t status, void *ctx) {
+        if (status == READ_OK) reinterpret_cast<Context *>(ctx)->read_buffer.push_back(byte);
     }, &ctx);
 
     for (uint8_t byte = 0; byte < 255; ++byte) {
-        ctx.decoder.reset_decode_state(false);
-        ctx.decoded.clear();
-        ctx.encoded = {0x7D, byte};
-        auto status = ctx.decoder.decode(ctx.encoded.data(), ctx.encoded.size());
+        ctx.reader.reset_read_state(false);
+        ctx.read_buffer.clear();
+        ctx.write_buffer = {0x7D, byte};
+        auto status = ctx.reader.read(ctx.write_buffer.data(), ctx.write_buffer.size());
 
         if (byte not_eq 0x5D and byte not_eq 0x5E) {
-            EXPECT_EQ(ctx.decoded.size(), 0);
+            EXPECT_EQ(ctx.read_buffer.size(), 0);
         } else {
-            EXPECT_EQ(status, DECODE_OK);
-            EXPECT_EQ(ctx.decoded.size(), 1);
-            EXPECT_EQ(ctx.decoded[0], byte ^ 0x20);
+            EXPECT_EQ(status, READ_OK);
+            EXPECT_EQ(ctx.read_buffer.size(), 1);
+            EXPECT_EQ(ctx.read_buffer[0], byte ^ 0x20);
         }
     }
 
 }
 
-TEST(ppp_encoding_test, decoding_normal_test) {
+TEST(ppp_pipelines_tests, decoding_normal_test) {
     struct Context {
-        PPPDecoder decoder;
-        std::vector<uint8_t> encoded;
-        std::vector<uint8_t> decoded;
+        PPP::PPPDecoder reader;
+        std::vector<uint8_t> write_buffer;
+        std::vector<uint8_t> read_buffer;
         std::vector<uint8_t> correct;
     } ctx;
 
-    ctx.encoded = {0x00, 0x01, 0x7E};
+    ctx.write_buffer = {0x00, 0x01, 0x7E};
     ctx.correct = {0x00, 0x01};
-    start_test_decoder(ctx.decoder, ctx.encoded, ctx.correct);
+    start_test_reader(ctx.reader, ctx.write_buffer, ctx.correct);
 
-    ctx.decoder.set_data_handler([](uint8_t byte, decode_status_t status, void *ctx) {
+    ctx.reader.set_data_handler([](uint8_t byte, read_status_t status, void *ctx) {
         auto &context = *reinterpret_cast<Context *>(ctx);
         switch (status) {
-            case DECODE_OK:
-                context.decoded.push_back(byte);
+            case READ_OK:
+                context.read_buffer.push_back(byte);
                 break;
-            case DECODE_END:
+            case READ_END:
                 break;
-            case DECODE_ERROR:
-            case UNKNOWN_DECODER_ERROR:
+            case READ_ERROR:
+            case UNKNOWN_READER_ERROR:
                 FAIL() << "An unforeseen mistake arose";
         }
     }, &ctx);
 
-    ctx.encoded = {0x7E, 0x7E, 0x00, 0x01, 0x7E};
+    ctx.write_buffer = {0x7E, 0x7E, 0x00, 0x01, 0x7E};
     ctx.correct = {0x00, 0x01};
-    ctx.decoder.decode(ctx.encoded.data(), ctx.encoded.size());
-    EXPECT_EQ(is_equals(ctx.decoded, ctx.correct), true);
+    ctx.reader.read(ctx.write_buffer.data(), ctx.write_buffer.size());
+    EXPECT_EQ(is_equals(ctx.read_buffer, ctx.correct), true);
 }
