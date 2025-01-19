6695deea923e497e2de6e894d61582519ccd9fb4
Arthur Koba <caprimores@gmail.com>
2025-01-19 05:39:53 +0300

    added Platformio support for new architecture

--- include/BDSP/BDSP.h
+++ include/BDSP/BDSP.h
@@ -1,10 +1,19 @@
 #pragma once
 
-#include "BDSP/export.h"
+#if defined(PLATFORMIO)
 #include "BDSP/types.h"
 #include "BDSP/packet.h"
-
+#include <BDSP/receiver.h>
+#include <BDSP/transmitter.h>
+#include <BDSP/transceiver.h>
+#include <BDSP/streams/COBS/reader.h>
+#include <BDSP/streams/COBS/writer.h>
+#include <BDSP/streams/PPP/reader.h>
+#include <BDSP/streams/PPP/writer.h>
+#else
+#include "BDSP/export.h"
 namespace BDSP {
 
 //BDSP_EXPORT int add(int a, int b);
 } // namespace BDSP
+#endif
