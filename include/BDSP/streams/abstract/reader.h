4d29b4e4b36cf0a7ec59aaebcc11e5c1c0af1de7
Arthur Koba <caprimores@gmail.com>
2025-01-19 04:43:20 +0300

    renamed delimiter to delimiter

--- include/BDSP/streams/abstract/reader.h
+++ include/BDSP/streams/abstract/reader.h
@@ -9,7 +9,7 @@ namespace BDSP::streams::ABS {
     class AbstractReader : public IReader {
         stream_data_handler_t _data_handler = nullptr;
         void *_data_handler_context = nullptr;
-        bool _is_waiting_for_the_delemiter = false;
+        bool _is_waiting_for_the_delimiter = false;
     protected:
 
         virtual read_status_t _process_byte(uint8_t byte) = 0;
@@ -24,7 +24,7 @@ namespace BDSP::streams::ABS {
 
         read_status_t read(uint8_t *buffer_ptr, size_t size) final;
 
-        void reset_read_state(bool is_need_wait_delemiter) final;
+        void reset_read_state(bool is_need_wait_delimiter) final;
 
         void set_stream_data_handler(stream_data_handler_t handler, void *context_ptr) final;
     };
--- src/streams/abstract/reader.cpp
+++ src/streams/abstract/reader.cpp
@@ -9,7 +9,7 @@ void AbstractReader::set_stream_data_handler(stream_data_handler_t handler, void
 }
 
 void AbstractReader::_handler(uint8_t byte, read_status_t status) {
-    if (_is_waiting_for_the_delemiter) return;
+    if (_is_waiting_for_the_delimiter) return;
     _data_handler(byte, status, _data_handler_context);
 }
 
@@ -38,7 +38,7 @@ read_status_t AbstractReader::read(uint8_t *buffer_ptr, size_t size) {
     return status;
 }
 
-void AbstractReader::reset_read_state(bool is_need_wait_delemiter) {
-    _is_waiting_for_the_delemiter = is_need_wait_delemiter;
+void AbstractReader::reset_read_state(bool is_need_wait_delimiter) {
+    _is_waiting_for_the_delimiter = is_need_wait_delimiter;
     _reset();
 }
