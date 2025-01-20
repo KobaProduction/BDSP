6b459f0e500e429d27f659e87c8817f2c78f437b
Arthur Koba <caprimores@gmail.com>
2025-01-20 07:13:00 +0300

    renamed need_wait_delemiter to need_wait_delimiter

--- include/BDSP/streams/types.h
+++ include/BDSP/streams/types.h
@@ -5,43 +5,36 @@
 
 namespace BDSP::streams {
 
-    typedef void (*stream_writer_t)(uint8_t byte, void *context_ptr);
+typedef void (*stream_writer_t)(uint8_t byte, void *context_ptr);
 
-    enum write_status_t {
-        WRITE_OK, WRITE_FINISH, UNKNOWN_WRITER_ERROR
-    };
+enum write_status_t { WRITE_OK, WRITE_FINISH, UNKNOWN_WRITER_ERROR };
 
-    class IWriter {
-    public:
-        virtual write_status_t write(uint8_t byte) = 0;
+class IWriter {
+public:
+    virtual write_status_t write(uint8_t byte) = 0;
 
-        virtual write_status_t write(uint8_t *buffer_ptr, size_t size) = 0;
+    virtual write_status_t write(uint8_t *buffer_ptr, size_t size) = 0;
 
-        virtual write_status_t finish() = 0;
+    virtual write_status_t finish() = 0;
 
-        virtual void set_stream_writer(stream_writer_t writer, void *context_ptr) = 0;
+    virtual void set_stream_writer(stream_writer_t writer, void *context_ptr) = 0;
+};
 
-    };
+enum read_status_t { READ_OK, READ_END, READ_ERROR, UNKNOWN_READER_ERROR };
 
+typedef void (*stream_data_handler_t)(uint8_t byte, read_status_t status, void *context_ptr);
 
-    enum read_status_t {
-        READ_OK, READ_END, READ_ERROR, UNKNOWN_READER_ERROR
-    };
+class IReader {
+public:
+    virtual void set_stream_data_handler(stream_data_handler_t handler, void *context_ptr) = 0;
 
-    typedef void (*stream_data_handler_t)(uint8_t byte, read_status_t status, void *context_ptr);
+    virtual void reset_read_state(bool need_wait_delimiter) = 0;
 
-    class IReader {
-    public:
-        virtual void set_stream_data_handler(stream_data_handler_t handler, void *context_ptr) = 0;
+    virtual read_status_t read(uint8_t byte) = 0;
 
-        virtual void reset_read_state(bool need_wait_delemiter) = 0;
+    virtual read_status_t read(uint8_t *buffer_ptr, size_t size) = 0;
+};
 
-        virtual read_status_t read(uint8_t byte) = 0;
+} // namespace BDSP::streams
 
-        virtual read_status_t read(uint8_t *buffer_ptr, size_t size) = 0;
-
-    };
-
-}
-
-#endif //BDSP_STREAMS_TYPES_H
+#endif // BDSP_STREAMS_TYPES_H
