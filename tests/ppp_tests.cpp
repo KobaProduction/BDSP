3ea1b2b8541b4762c23a05d38774162fb30b51fd
Arthur Koba <caprimores@gmail.com>
2025-01-25 18:34:29 +0300

    refactor ppp tests, ppp code coverage 100%

--- tests/ppp_tests.cpp
+++ tests/ppp_tests.cpp
@@ -1,80 +1,70 @@
-#include <cstdint>
-#include <vector>
 #include <gtest/gtest.h>
+#include <vector>
 
-#include <BDSP/streams/PPP/writer.h>
+#include <BDSP/streams/PPP/checkers.h>
 #include <BDSP/streams/PPP/reader.h>
+#include <BDSP/streams/PPP/writer.h>
 
 #include "utils/testing.h"
 
 using namespace BDSP::streams;
+using namespace BDSP::streams::PPP;
+using namespace BDSP::streams::PPP::core;
 
+TEST(ppp_pipelines_tests, set_config_checker_test) {
+    ppp_config_t config = {0x7E, 0x7D, 0x20};
+    EXPECT_EQ(check_ppp_config(config), SET_OK);
+
+    config = {0x7E, 0x7D, 0x00};
+    EXPECT_EQ(check_ppp_config(config), ERROR_ESCAPE_MASK_IS_NULL);
+
+    config = {0x7E, 0x7E, 0x20};
+    EXPECT_EQ(check_ppp_config(config), ERROR_END_AND_ESCAPE_BYTES_IS_EQUALS);
+
+    config = {0x7E, 0x5E, 0x20};
+    EXPECT_EQ(check_ppp_config(config), ERROR_INVERTED_END_AND_ESCAPE_BYTES_IS_EQUALS);
+}
+
+TEST(ppp_pipelines_tests, set_config_test) {
+    PPPWriter writer;
+    PPPReader reader;
+    ppp_config_t config = writer.get_config();
+
+    EXPECT_EQ(reader.set_config(config), SET_OK);
+    EXPECT_EQ(writer.set_config(config), SET_OK);
+
+    reader.set_stream_data_handler([](uint8_t byte, read_status_t status, void *ctx) { }, nullptr);
+
+    EXPECT_EQ(reader.read(config.escape_byte), READ_OK);
+    EXPECT_EQ(reader.set_config(config), ERROR_PROCESS_NOT_FINISHED);
+    reader.reset_read_state(false);
+
+    config.escape_byte = config.end_byte;
+
+    EXPECT_EQ(reader.set_config(config), ERROR_END_AND_ESCAPE_BYTES_IS_EQUALS);
+}
 
 TEST(ppp_pipelines_tests, encoding_test) {
-    PPP::PPPWriter writer;
+    PPPWriter writer;
+    PPPReader reader;
 
     std::vector<uint8_t> data = {0x7E, 0x7D, 0x00};
-    std::vector<uint8_t> correct = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
+    std::vector<uint8_t> correct_encoded = {0x7D, 0x5E, 0x7D, 0x5D, 0x00, 0x7E};
 
-    start_test_writer(writer, data, correct, true);
+    start_test_writer(writer, data, correct_encoded, true);
+    start_test_reader(reader, correct_encoded, data, true);
 }
 
 TEST(ppp_pipelines_tests, decoding_errors_test) {
-    struct Context {
-        PPP::PPPDecoder reader;
-        std::vector<uint8_t> read_buffer;
-        std::vector<uint8_t> write_buffer;
-    } ctx;
+    PPPReader reader;
+    ppp_config_t config = reader.get_config();
 
-    ctx.reader.set_stream_data_handler([](uint8_t byte, read_status_t status, void *ctx) {
-        if (status == READ_OK) reinterpret_cast<Context *>(ctx)->read_buffer.push_back(byte);
-    }, &ctx);
+    reader.set_stream_data_handler([](uint8_t byte, read_status_t status, void *ctx) { }, nullptr);
 
-    for (uint8_t byte = 0; byte < 255; ++byte) {
-        ctx.reader.reset_read_state(false);
-        ctx.read_buffer.clear();
-        ctx.write_buffer = {0x7D, byte};
-        auto status = ctx.reader.read(ctx.write_buffer.data(), ctx.write_buffer.size());
+    EXPECT_EQ(reader.read(config.escape_byte), READ_OK);
+    EXPECT_EQ(reader.read(config.end_byte), READ_ERROR);
+    reader.reset_read_state(false);
 
-        if (byte not_eq 0x5D and byte not_eq 0x5E) {
-            EXPECT_EQ(ctx.read_buffer.size(), 0);
-        } else {
-            EXPECT_EQ(status, READ_OK);
-            EXPECT_EQ(ctx.read_buffer.size(), 1);
-            EXPECT_EQ(ctx.read_buffer[0], byte ^ 0x20);
-        }
-    }
-
-}
-
-TEST(ppp_pipelines_tests, decoding_normal_test) {
-    struct Context {
-        PPP::PPPDecoder reader;
-        std::vector<uint8_t> write_buffer;
-        std::vector<uint8_t> read_buffer;
-        std::vector<uint8_t> correct;
-    } ctx;
-
-    ctx.write_buffer = {0x00, 0x01, 0x7E};
-    ctx.correct = {0x00, 0x01};
-    start_test_reader(ctx.reader, ctx.write_buffer, ctx.correct);
-
-    ctx.reader.set_stream_data_handler([](uint8_t byte, read_status_t status, void *ctx) {
-        auto &context = *reinterpret_cast<Context *>(ctx);
-        switch (status) {
-            case READ_OK:
-                context.read_buffer.push_back(byte);
-                break;
-            case READ_END:
-                break;
-            case READ_ERROR:
-            case UNKNOWN_READER_ERROR:
-                FAIL() << "An unforeseen mistake arose";
-        }
-    }, &ctx);
-
-    ctx.write_buffer = {0x7E, 0x7E, 0x00, 0x01, 0x7E};
-    ctx.correct = {0x00, 0x01};
-    ctx.reader.read(ctx.write_buffer.data(), ctx.write_buffer.size());
-    EXPECT_EQ(is_equals(ctx.read_buffer, ctx.correct), true);
+    EXPECT_EQ(reader.read(config.escape_byte), READ_OK);
+    EXPECT_EQ(reader.read(0), READ_ERROR);
 }
