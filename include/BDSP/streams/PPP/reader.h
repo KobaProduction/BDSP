9088b3e222b298dd3a01a992d03a0fe3acdd7c29
Arthur Koba <caprimores@gmail.com>
2025-01-25 18:33:56 +0300

    refactor ppp reader

--- include/BDSP/streams/PPP/reader.h
+++ include/BDSP/streams/PPP/reader.h
@@ -3,20 +3,28 @@
 
 #include <stdint.h>
 
+#include "BDSP/streams/PPP/types.h"
 #include "BDSP/streams/abstract/reader.h"
 
 namespace BDSP::streams::PPP {
 
-    class PPPDecoder final : public ABS::AbstractReader {
-    private:
-        void _reset() override;
-    protected:
-        uint8_t _escape_byte = 0x7D;
-        uint8_t _end_byte = 0x7E;
-        uint8_t _escape_mask = 0x20;
-        bool _is_escape_state = false;
+namespace core {
+class PPPReaderCore: public ABS::AbstractReader {
+protected:
+    ppp_config_t _cfg;
+    bool _is_escape_state = false;
 
-        streams::read_status_t _process_byte(uint8_t byte) override;
-    };
-}
+    read_status_t _process_byte(uint8_t byte) override;
+    void _reset() override;
+
+public:
+    ppp_config_t get_config();
+    set_ppp_config_status set_config(ppp_config_t config);
+};
+} // namespace core
+
+class PPPReader final: public core::PPPReaderCore { };
+
+} // namespace BDSP::streams::PPP
+
 #endif // BDSP_STREAMS_PPP_READER_H
--- src/streams/PPP/reader.cpp
+++ src/streams/PPP/reader.cpp
@@ -1,29 +1,47 @@
 #include "BDSP/streams/PPP/reader.h"
+#include "BDSP/streams/PPP/checkers.h"
 
 using namespace BDSP::streams;
 using namespace BDSP::streams::PPP;
+using namespace BDSP::streams::PPP::core;
 
-void PPPDecoder::_reset() {
+void PPPReaderCore::_reset() {
     _is_escape_state = false;
 }
 
-read_status_t PPPDecoder::_process_byte(uint8_t byte) {
+read_status_t PPPReaderCore::_process_byte(uint8_t byte) {
     read_status_t status = READ_OK;
 
-    if (byte == _end_byte) {
+    if (byte == _cfg.end_byte) {
         return _is_escape_state ? READ_ERROR : READ_END;
     }
 
     if (_is_escape_state) {
         _is_escape_state = false;
-        byte ^= _escape_mask;
-        if (byte not_eq _escape_byte and byte not_eq _end_byte) {
+        byte ^= _cfg.escape_mask;
+        if (byte not_eq _cfg.escape_byte and byte not_eq _cfg.end_byte) {
             return READ_ERROR;
         }
-    } else if (byte == _escape_byte) {
+    } else if (byte == _cfg.escape_byte) {
         _is_escape_state = true;
         return READ_OK;
     }
     _handler(byte, status);
+    return status;
+}
+
+ppp_config_t PPPReaderCore::get_config() {
+    return _cfg;
+}
+
+set_ppp_config_status PPPReaderCore::set_config(ppp_config_t config) {
+    set_ppp_config_status status = check_ppp_config(config);
+    if (_is_escape_state) {
+        status = ERROR_PROCESS_NOT_FINISHED;
+    }
+    if (status == SET_OK) {
+        _cfg = config;
+    }
+    _set_ready_state(status == SET_OK);
     return status;
 }
