e6575e4ae6429fe946e89cb1b7ebeb5bf6335acf
Arthur Koba <caprimores@gmail.com>
2025-01-26 15:44:25 +0300

    update bdsp example

--- examples/Desktop/src/BDSP_EXAMPLE.cpp
+++ examples/Desktop/src/BDSP_EXAMPLE.cpp
@@ -8,20 +8,24 @@
 #include "utils.h"
 
 using namespace BDSP;
-using namespace BDSP::streams;
+using namespace BDSP::streams::COBS;
 
 
 int main() {
     std::vector<uint8_t> data;
-    for (int i = 0; i < 10; ++i) data.push_back(i);
-    Packet packet(0, data.size(), data.data());
+    for (int i = 1; i < 10; ++i) data.push_back(i);
+    bdsp_packet_t packet;
+
+//    packet.id = 0;
+    packet.size = data.size();
+    packet.data_ptr = data.data();
 
     BDSPTransceiver transceiver;
 
-    COBS::COBSWriter writer;
+    COBSWriter writer;
     transceiver.set_writer(&writer);
 
-    COBS::COBSReader reader;
+    COBSReader reader;
     transceiver.set_reader(&reader);
 
     writer.set_stream_writer([](uint8_t byte, void *ctx) {
@@ -30,15 +34,18 @@ int main() {
         //
         // In real-world use, you need to read a callback function that takes data from a communication channel and
         // passes it to a BDSPTransceiver or BDSPReceiver or configured decoder.
+//        std::cout << uint32_t(byte) << " ";
         reinterpret_cast<BDSPTransceiver *>(ctx)->parse(byte);
     }, &transceiver);
 
-    transceiver.set_packet_handler([] (Packet &packet, void *ctx) {
-        std::cout << "Got packet! Id: " << static_cast<uint32_t>(packet.id) << std::endl << "Packet ";
-        show_data_(packet.data_ptr, packet.size);
+    transceiver.set_packet_handler([] (bdsp_packet_context_t &packet_ctx, void *ctx) {
+        std::cout << "Got packet! Id: " << static_cast<uint32_t>(packet_ctx.packet_id);
+        std:: cout << ", size: " << uint32_t(packet_ctx.packet.size) << std::endl;
+        std:: cout << "Packet ";
+        show_data_(packet_ctx.packet.data_ptr, packet_ctx.packet.size);
     }, nullptr);
 
-    transceiver.send_packet(packet);
+    transceiver.send_packet(1, packet);
 
     return 0;
 }
