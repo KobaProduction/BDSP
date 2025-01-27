31fb2516fc4549abf9e3563a58b45cf6a7aae7ce
Arthur Koba <caprimores@gmail.com>
2025-01-27 17:58:23 +0300

    refactored crc8

--- include/BDSP/checksums/crc8.h
+++ include/BDSP/checksums/crc8.h
@@ -1,20 +1,27 @@
-#ifndef BDSP_CHECKSUMS_CRC_CRC8_H
-#define BDSP_CHECKSUMS_CRC_CRC8_H
+#ifndef BDSP_CHECKSUMS_CRC8_H
+#define BDSP_CHECKSUMS_CRC8_H
 
 #include <stdint.h>
 #include <stdlib.h>
 
 namespace BDSP::checksums {
-    /**
-     * Varieties of checksum function CRC-8/NRSC-5
-     *
-     * @param data_ptr a pointer to the data for which the calculation will be carried out..
-     * @param size number of bytes of data.
-     * @param crc is used to continue the calculation when it occurs in parts. Submit to Base the past value of the control amount..
-     * @return crs8 sum value.
-     */
-    uint8_t crc8_nrsc5(const uint8_t *data_ptr, size_t size, uint8_t crc = 0xff);
 
-    uint8_t crc8_lte(const uint8_t *data_ptr, size_t size, uint8_t crc = 0x00);
+namespace core {
+uint8_t crc8(const uint8_t *data_ptr, size_t size, uint8_t crc, uint8_t poly);
 }
-#endif //BDSP_CHECKSUMS_CRC_CRC8_H
+
+uint8_t crc8_lte(const uint8_t *data_ptr, size_t size, uint8_t crc = 0x00);
+
+/**
+ * Varieties of checksum function CRC-8/NRSC-5
+ *
+ * @param data_ptr a pointer to the data for which the calculation will be carried out..
+ * @param size number of bytes of data.
+ * @param crc is used to continue the calculation when it occurs in parts. Submit to Base the past value of the control
+ * amount..
+ * @return crs8 sum value.
+ */
+uint8_t crc8_nrsc5(const uint8_t *data_ptr, size_t size, uint8_t crc = 0xff);
+} // namespace BDSP::checksums
+
+#endif // BDSP_CHECKSUMS_CRC8_H
--- src/checksums/crc8.cpp
+++ src/checksums/crc8.cpp
@@ -3,13 +3,13 @@
 using namespace BDSP;
 using namespace BDSP::checksums;
 
-uint8_t checksums::crc8_nrsc5(const uint8_t *data_ptr, size_t size, uint8_t crc) {
+uint8_t core::crc8(const uint8_t *data_ptr, size_t size, uint8_t crc, uint8_t poly) {
     size_t i, j;
     for (i = 0; i < size; i++) {
         crc ^= data_ptr[i];
         for (j = 0; j < 8; j++) {
             if (crc & 0x80) {
-                crc = uint8_t(((crc << 1) ^ 0x31));
+                crc = uint8_t(((crc << 1) ^ poly));
             } else {
                 crc <<= 1;
             }
@@ -19,16 +19,9 @@ uint8_t checksums::crc8_nrsc5(const uint8_t *data_ptr, size_t size, uint8_t crc)
 }
 
 uint8_t checksums::crc8_lte(const uint8_t *data_ptr, size_t size, uint8_t crc) {
-    size_t i, j;
-    for (i = 0; i < size; i++) {
-        crc ^= data_ptr[i];
-        for (j = 0; j < 8; j++) {
-            if (crc & 0x80) {
-                crc = uint8_t(((crc << 1) ^ 0x9B));
-            } else {
-                crc <<= 1;
-            }
-        }
-    }
-    return crc;
+    return core::crc8(data_ptr, size, crc, 0x9B);
+}
+
+uint8_t checksums::crc8_nrsc5(const uint8_t *data_ptr, size_t size, uint8_t crc) {
+    return core::crc8(data_ptr, size, crc, 0x31);
 }
