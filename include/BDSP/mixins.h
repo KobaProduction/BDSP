ee13042f0c639ba14747e2f41ae5cb1330ebd3e0
Arthur Koba <caprimores@gmail.com>
2025-01-27 17:22:42 +0300

    added BDSPV1ChecksumMixin

--- include/BDSP/mixins.h
+++ include/BDSP/mixins.h
@@ -2,24 +2,36 @@
 #define BDSP_MIXINS_H
 
 #include "BDSP/types.h"
+#include "BDSP/checksums/crc/crc8.h"
 
 #define DEFAULT_BDSP_MAX_PACKET_SIZE 65535
 
 namespace BDSP::core {
 
-    enum set_max_packet_size_status_t {
-        MAX_PACKET_SIZE_SET,
-        INVALID_MAXIMUM_PACKET_SIZE
-    };
+enum set_max_packet_size_status_t { MAX_PACKET_SIZE_SET, INVALID_MAXIMUM_PACKET_SIZE };
 
-    class MaxPacketSizeMixin {
-    public:
-        set_max_packet_size_status_t set_max_packet_size(uint16_t max_packet_size);
-    protected:
-        uint16_t _max_packet_size = DEFAULT_BDSP_MAX_PACKET_SIZE;
-    };
+class MaxPacketSizeMixin {
+public:
+    set_max_packet_size_status_t set_max_packet_size(uint16_t max_packet_size);
 
-}
+protected:
+    uint16_t _max_packet_size = DEFAULT_BDSP_MAX_PACKET_SIZE;
+};
 
+typedef uint8_t (*checksum_function_t)(const uint8_t *data_ptr, size_t size, uint8_t base);
 
-#endif //BDSP_MIXINS_H
+enum set_checksum_function_status_t { SET_CHECKSUM_FUNCTION_OK, CHECKSUM_FUNCTION_NULL_POINTER_ERROR };
+
+class BDSPV1ChecksumMixin {
+    checksum_function_t _checksum_function = BDSP::utils::crc8;
+
+public:
+    set_checksum_function_status_t set_checksum_function(checksum_function_t _checksum_function);
+
+protected:
+    uint8_t _calc_checksum(bdsp_packet_v1_header header, uint8_t *data_ptr, uint16_t size);
+};
+
+} // namespace BDSP::core
+
+#endif // BDSP_MIXINS_H
--- src/mixins.cpp
+++ src/mixins.cpp
@@ -3,7 +3,33 @@
 using namespace BDSP::core;
 
 set_max_packet_size_status_t MaxPacketSizeMixin::set_max_packet_size(uint16_t max_packet_size) {
-    if (max_packet_size < 1) return INVALID_MAXIMUM_PACKET_SIZE;
+    if (max_packet_size < 1) {
+        return INVALID_MAXIMUM_PACKET_SIZE;
+    }
     _max_packet_size = max_packet_size;
     return MAX_PACKET_SIZE_SET;
+}
+
+uint8_t BDSPV1ChecksumMixin::_calc_checksum(bdsp_packet_v1_header header, uint8_t *data_ptr, uint16_t size) {
+    uint8_t checksum = _checksum_function(reinterpret_cast<uint8_t *>(&header), 1, 255);
+
+    uint8_t size_byte = size;
+
+    checksum = _checksum_function(&size_byte, 1, checksum);
+    if (header.two_bytes_for_packet_size_flag) {
+        size_byte = size >> 8;
+        checksum = _checksum_function(&size_byte, 1, checksum);
+    }
+
+    checksum = _checksum_function(data_ptr, size, checksum);
+
+    return checksum;
+}
+
+set_checksum_function_status_t BDSPV1ChecksumMixin::set_checksum_function(checksum_function_t checksum_function) {
+    if (checksum_function not_eq nullptr) {
+        _checksum_function = checksum_function;
+        return SET_CHECKSUM_FUNCTION_OK;
+    }
+    return CHECKSUM_FUNCTION_NULL_POINTER_ERROR;
 }
