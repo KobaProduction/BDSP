8d6c31e7f153954b7f27cd8b88bf1f269013b348
Arthur Koba <caprimores@gmail.com>
2025-01-31 18:07:27 +0300

    update example

--- examples/Desktop/src/BDSP_EXAMPLE.cpp
+++ examples/Desktop/src/BDSP_EXAMPLE.cpp
@@ -1,51 +1,55 @@
 #include <iostream>
 #include <vector>
 
-#include <BDSP/transceiver.h>
-#include <BDSP/streams/COBS/writer.h>
 #include <BDSP/streams/COBS/reader.h>
+#include <BDSP/streams/COBS/writer.h>
+#include <BDSP/transceiver.h>
 
 #include "utils.h"
 
 using namespace BDSP;
 using namespace BDSP::streams::COBS;
 
-
 int main() {
     std::vector<uint8_t> data;
-    for (int i = 1; i < 10; ++i) data.push_back(i);
-    bdsp_packet_t packet;
+    for (int i = 1; i < 10; ++i)
+        data.push_back(i);
+    bdsp_packet_context_t packet_context;
 
-//    packet.id = 0;
-    packet.size = data.size();
-    packet.data_ptr = data.data();
+    packet_context.packet_id = 0;
+    packet_context.size = data.size();
+    packet_context.data_ptr = data.data();
 
     BDSPTransceiver transceiver;
 
     COBSWriter writer;
-    transceiver.set_writer(&writer);
+    transceiver.set_stream_writer(&writer);
 
     COBSReader reader;
-    transceiver.set_reader(&reader);
+    transceiver.set_stream_reader(&reader);
 
-    writer.set_stream_writer([](uint8_t byte, void *ctx) {
-        // This lambda function implements the sending of encoded data to the communication channel.
-        // But for the current example, they are sent directly to the BDSPTransceiver, forming pipeline.
-        //
-        // In real-world use, you need to read a callback function that takes data from a communication channel and
-        // passes it to a BDSPTransceiver or BDSPReceiver or configured decoder.
-//        std::cout << uint32_t(byte) << " ";
-        reinterpret_cast<BDSPTransceiver *>(ctx)->parse(byte);
-    }, &transceiver);
+    writer.set_stream_writer(
+        [](uint8_t byte, void *ctx) {
+            reinterpret_cast<streams::IReader *>(ctx)->read(byte);
+        },
+        &reader);
 
-    transceiver.set_packet_handler([] (bdsp_packet_context_t &packet_ctx, void *ctx) {
-        std::cout << "Got packet! Id: " << static_cast<uint32_t>(packet_ctx.packet_id);
-        std:: cout << ", size: " << uint32_t(packet_ctx.packet.size) << std::endl;
-        std:: cout << "Packet ";
-        show_data_(packet_ctx.packet.data_ptr, packet_ctx.packet.size);
-    }, nullptr);
+    transceiver.set_packet_handler(
+        [](bdsp_packet_context_t &packet_ctx, void *ctx) {
+            std::cout << "Got packet! Id: " << static_cast<uint32_t>(packet_ctx.packet_id);
+            std::cout << ", size: " << uint32_t(packet_ctx.size) << std::endl;
+            std::cout << "Packet ";
+            show_data_(packet_ctx.data_ptr, packet_ctx.size);
+        },
+        nullptr);
 
-    transceiver.send_packet(1, packet);
+    transceiver.set_error_handler(
+        [](bdsp_receiver_parse_status_t status, void *ctx) {
+            std::cout << "bdsp_receiver_parse_error_status_t: " << static_cast<uint32_t>(status) << std::endl;
+        },
+        nullptr);
+
+    transceiver.send_packet(packet_context);
 
     return 0;
 }
