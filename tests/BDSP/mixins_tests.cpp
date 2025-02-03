3abfce4e87cbd155d8dbf73939a5d91dbb2e45c8
Arthur Koba <caprimores@gmail.com>
2025-02-03 22:43:52 +0300

    fixed errors in tests after change ordering bits in bdsp packet_v1_header

--- tests/BDSP/mixins_tests.cpp
+++ tests/BDSP/mixins_tests.cpp
@@ -4,6 +4,7 @@
 #include "BDSP/mixins.h"
 
 using namespace BDSP::core;
+using namespace BDSP::checksums;
 
 TEST(bdsp_core_tests, max_packet_size_mixin_test) {
     MaxPacketSizeMixin mixin;
@@ -25,7 +26,7 @@ TEST(bdsp_core_tests, bdsp_v1_checksum_mixin_test) {
     ChecksumTester tester;
 
     EXPECT_EQ(tester.set_checksum_function(nullptr), CHECKSUM_FUNCTION_NULL_POINTER_ERROR);
-    EXPECT_EQ(tester.set_checksum_function(BDSP::checksums::crc8_nrsc5), SET_CHECKSUM_FUNCTION_OK);
+    EXPECT_EQ(tester.set_checksum_function(crc8_nrsc5), SET_CHECKSUM_FUNCTION_OK);
 
     packet_v1_header header{};
 
@@ -39,14 +40,28 @@ TEST(bdsp_core_tests, bdsp_v1_checksum_mixin_test) {
     size_t data_size = 1;
     auto *data = new uint8_t[data_size];
     for (int i = 0; i < data_size; ++i) data[i] = 0xAA;
-    EXPECT_EQ(tester.calc(header, data, data_size), 0x15);
+
+    uint8_t correct_checksum = crc8_nrsc5(reinterpret_cast<uint8_t*>(&header), 1);
+    uint8_t buffer = data_size;
+    correct_checksum = crc8_nrsc5(&buffer, 1, correct_checksum);
+    correct_checksum = crc8_nrsc5(data, data_size, correct_checksum);
+
+
+    EXPECT_EQ(tester.calc(header, data, data_size), correct_checksum);
     delete [] data;
 
     data_size = 300;
     data = new uint8_t[data_size];
     for (int i = 0; i < data_size; ++i) data[i] = 0xAA;
     header.is_two_bytes_for_packet_size = true;
-    EXPECT_EQ(tester.calc(header, data, data_size), 0x45);
+
+    correct_checksum = crc8_nrsc5(reinterpret_cast<uint8_t*>(&header), 1);
+    buffer = data_size;
+    correct_checksum = crc8_nrsc5(&buffer, 1, correct_checksum);
+    buffer = data_size >> 8;
+    correct_checksum = crc8_nrsc5(&buffer, 1, correct_checksum);
+    correct_checksum = crc8_nrsc5(data, data_size, correct_checksum);
+    EXPECT_EQ(tester.calc(header, data, data_size), correct_checksum);
     delete [] data;
 
     header.is_checksum_used = false;
