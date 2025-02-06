6bfb755f1de1b8829f653f8c3299e4a6598ca82d
Arthur Koba <caprimores@gmail.com>
2025-02-06 14:54:23 +0300

    released VectorTestReader functionality

--- tests/utils/vector_test_streams.cpp
+++ tests/utils/vector_test_streams.cpp
@@ -1,5 +1,7 @@
 #include "vector_test_streams.h"
 
+using namespace BDSP::streams;
+
 void VectorTestWriter::_process_byte(uint8_t byte) {
     if (stream_data.empty()) {
         stream_data.emplace_back();
@@ -19,10 +21,34 @@ VectorTestWriter::VectorTestWriter() {
     set_stream_writer([] (uint8_t byte, void *ctx) {}, nullptr);
 }
 
-BDSP::streams::read_status_t VectorTestReader::_process_byte(uint8_t byte) {
-    return BDSP::streams::STREAM_READ_END;
+read_status_t VectorTestReader::read(uint8_t byte, read_status_t status) {
+    if (not _data_handler) return STREAM_READER_NOT_READY_ERROR;
+    _data_handler(byte, status, _data_handler_context);
+    return status;
 }
 
-void VectorTestReader::_reset() {
+read_status_t VectorTestReader::read(uint8_t byte) {
+    if (not _data_handler) return STREAM_READER_NOT_READY_ERROR;
+    _data_handler(byte, STREAM_READ_OK, _data_handler_context);
+    return STREAM_READ_OK;
+}
 
+read_status_t VectorTestReader::read(uint8_t *buffer_ptr, size_t size) {
+    for (int i = 0; i < size; ++i) {
+        read(buffer_ptr[i], STREAM_READ_OK);
+    }
+    return STREAM_READ_OK;
+}
+
+void VectorTestReader::reset_read_state(bool need_wait_delimiter) {
+
+}
+
+void VectorTestReader::set_stream_data_handler(stream_data_handler_t handler, void *context_ptr) {
+    _data_handler = handler;
+    _data_handler_context = context_ptr;
+}
+
+bool VectorTestReader::get_ready_status() {
+    return bool(_data_handler);
 }
--- tests/utils/vector_test_streams.h
+++ tests/utils/vector_test_streams.h
@@ -16,11 +16,20 @@ public:
     VectorTestWriter();
 };
 
-class VectorTestReader final: public BDSP::streams::ABS::AbstractReader {
-    BDSP::streams::read_status_t _process_byte(uint8_t byte) override;
-    void _reset() override;
+class VectorTestReader final: public BDSP::streams::IStreamReader {
 public:
-    std::vector<std::vector<uint8_t>> stream_data;
+    BDSP::streams::stream_data_handler_t _data_handler = nullptr;
+    void *_data_handler_context = nullptr;
+
+    BDSP::streams::read_status_t read(uint8_t byte) override;
+    BDSP::streams::read_status_t read(uint8_t *buffer_ptr, size_t size) override;
+    void reset_read_state(bool need_wait_delimiter) override;
+    void set_stream_data_handler(BDSP::streams::stream_data_handler_t handler, void *context_ptr) override;
+    bool get_ready_status() override;
+
+    bool ready_status = true;
+
+    BDSP::streams::read_status_t read(uint8_t byte, BDSP::streams::read_status_t status);
 };
 
 
