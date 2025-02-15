ea039ba0485dacd4897be2222a7cba02ff160a79
Arthur Koba <caprimores@gmail.com>
2025-02-15 03:07:37 +0300

    released StreamWriter has been uses IStreamWritingStrategy

---
+++ include/BDSP/streams/writer.h
@@ -0,0 +1,40 @@
+#ifndef BDSP_STREAMS_STREAM_WRITER_H
+#define BDSP_STREAMS_STREAM_WRITER_H
+
+#include "BDSP/streams/abstract/mixins.h"
+#include "BDSP/streams/types.h"
+
+namespace BDSP::streams {
+
+class StreamWriterCore: public IStreamWriter, public BDSP::streams::core::StreamReadyMixin {
+private:
+    stream_writer_t _writer = nullptr;
+    void *_writer_context = nullptr;
+protected:
+    core::IStreamWritingStrategy *_strategy = nullptr;
+    void _set_strategy(core::IStreamWritingStrategy &strategy) noexcept;
+
+public:
+    bool get_ready_status() final;
+    write_status_t finish() final;
+    void set_stream_writer(stream_writer_t writer, void *context_ptr) final;
+    write_status_t write(uint8_t byte) final;
+    write_status_t write(uint8_t *buffer_ptr, size_t size) final;
+};
+
+template<typename TStrategy>
+class StreamWriter: public StreamWriterCore {
+public:
+    StreamWriter() {
+        _strategy = new TStrategy();
+        _set_strategy(*_strategy);
+    }
+
+    ~StreamWriter() { delete _strategy; }
+
+    TStrategy &get_strategy() { return *reinterpret_cast<TStrategy *>(_strategy); };
+};
+
+} // namespace BDSP::streams
+
+#endif // BDSP_STREAMS_STREAM_WRITER_H
---
+++ src/streams/writer.cpp
@@ -0,0 +1,57 @@
+#include "BDSP/streams/writer.h"
+
+using namespace BDSP::streams;
+using namespace BDSP::streams::core;
+
+void StreamWriterCore::_set_strategy(IStreamWritingStrategy &strategy) noexcept {
+    _strategy = &strategy;
+    _strategy->init(
+        [](uint8_t byte, void *ctx) {
+            auto *self = reinterpret_cast<StreamWriterCore *>(ctx);
+            self->_writer(byte, self->_writer_context);
+        },
+        [](bool new_ready_state, void *ctx) {
+            reinterpret_cast<StreamWriterCore *>(ctx)->_set_strategy_state(new_ready_state);
+        },
+        this
+    );
+}
+
+bool StreamWriterCore::get_ready_status() {
+    return _state == READY;
+}
+
+write_status_t StreamWriterCore::finish() {
+    if (_state not_eq READY) {
+        return STREAM_WRITER_NOT_READY_ERROR;
+    }
+    _strategy->finish();
+    return STREAM_WRITE_END;
+}
+
+void StreamWriterCore::set_stream_writer(stream_writer_t writer, void *context_ptr) {
+    _writer = writer;
+    _writer_context = context_ptr;
+    _set_handler_state(_writer not_eq nullptr);
+}
+
+write_status_t StreamWriterCore::write(uint8_t byte) {
+    if (_state not_eq READY) {
+        return STREAM_WRITER_NOT_READY_ERROR;
+    }
+    _strategy->write(byte);
+    return STREAM_WRITE_OK;
+}
+
+write_status_t StreamWriterCore::write(uint8_t *buffer_ptr, size_t size) {
+    write_status_t status = STREAM_WRITE_OK;
+    for (size_t i = 0; i < size; ++i) {
+        status = write(buffer_ptr[i]);
+        if (status not_eq STREAM_WRITE_OK) {
+            break;
+        }
+    }
+    return status;
+}
+
+
