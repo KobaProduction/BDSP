dfbcdf1398b48749f3ec7ae3a9b12e4d22b53200
Arthur Koba <caprimores@gmail.com>
2025-02-18 22:42:02 +0300

    refactored stream and stream strategies types and interfaces

--- include/BDSP/streams/types.h
+++ include/BDSP/streams/types.h
@@ -5,54 +5,34 @@
 
 namespace BDSP::streams {
 
-enum read_status_t { STREAM_READ_OK, STREAM_READ_END, STREAM_READ_ERROR, STREAM_READER_NOT_READY_ERROR };
+enum stream_read_status_t {
+    STREAM_READ_OK = 0,
+    STREAM_READ_DELIMITER = 1,
+    STREAM_READ_ERROR = 2,
+    STREAM_READER_NOT_READY_ERROR = 3
+};
+enum stream_write_status_t { STREAM_WRITE_OK, STREAM_WRITE_DELIMITER, STREAM_WRITER_NOT_READY_ERROR };
 
-typedef void (*stream_data_handler_t)(uint8_t byte, read_status_t status, void *context_ptr);
+typedef void (*stream_data_callback_t)(uint8_t byte, stream_read_status_t status, void *context_ptr);
+typedef void (*stream_write_handler_t)(uint8_t byte, void *context_ptr);
 
 class IStreamReader {
 public:
-    virtual read_status_t read(uint8_t byte) = 0;
-    virtual read_status_t read(uint8_t *buffer_ptr, size_t size) = 0;
+    virtual stream_read_status_t read(uint8_t byte) = 0;
+    virtual stream_read_status_t read(uint8_t *buffer_ptr, size_t size) = 0;
     virtual void reset_read_state(bool need_wait_delimiter) = 0;
-    virtual void set_stream_data_handler(stream_data_handler_t handler, void *context_ptr) = 0;
+    virtual void set_stream_data_handler(stream_data_callback_t handler, void *context_ptr) = 0;
     virtual bool get_ready_status() = 0;
 };
-
-enum write_status_t { STREAM_WRITE_OK, STREAM_WRITE_END, STREAM_WRITER_NOT_READY_ERROR };
-
-typedef void (*stream_writer_t)(uint8_t byte, void *context_ptr);
 
 class IStreamWriter {
 public:
-    virtual write_status_t finish() = 0;
-    virtual void set_stream_writer(stream_writer_t writer, void *context_ptr) = 0;
-    virtual write_status_t write(uint8_t byte) = 0;
-    virtual write_status_t write(uint8_t *buffer_ptr, size_t size) = 0;
+    virtual stream_write_status_t finish() = 0;
+    virtual void set_stream_writer(stream_write_handler_t writer, void *context_ptr) = 0;
+    virtual stream_write_status_t write(uint8_t byte) = 0;
+    virtual stream_write_status_t write(uint8_t *buffer_ptr, size_t size) = 0;
     virtual bool get_ready_status() = 0;
 };
-
-namespace core {
-
-typedef void (*strategy_ready_state_callback_t)(bool state, void *ctx);
-
-class IStreamWritingStrategy {
-public:
-    virtual ~IStreamWritingStrategy() = default;
-    virtual void init(stream_writer_t write_callback, strategy_ready_state_callback_t ready_callback, void *ctx) = 0;
-    virtual void finish() = 0;
-    virtual void send_delimiter() = 0;
-    virtual void write(uint8_t byte) = 0;
-};
-
-class IStreamReadingStrategy {
-public:
-    virtual void init(stream_data_handler_t read_callback,
-                      strategy_ready_state_callback_t ready_callback, void *ctx) = 0;
-    virtual read_status_t read(uint8_t byte) = 0;
-    virtual void reset_read_state() = 0;
-    virtual ~IStreamReadingStrategy() = default;
-};
-} // namespace core
 
 } // namespace BDSP::streams
 
