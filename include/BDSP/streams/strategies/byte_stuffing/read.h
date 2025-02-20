939d2ed775c2ea2367a0408e9143c6246d1359ee
Arthur Koba <caprimores@gmail.com>
2025-02-20 23:25:58 +0300

    added byte stuffing and ppp read strategy

---
+++ include/BDSP/streams/strategies/byte_stuffing/read.h
@@ -0,0 +1,39 @@
+#ifndef BDSP_STREAMS_STRATEGIES_BYTE_STUFFING_READ_STRATEGY_H
+#define BDSP_STREAMS_STRATEGIES_BYTE_STUFFING_READ_STRATEGY_H
+
+#include "stdint.h"
+
+#include "BDSP/streams/strategies/abstract/read.h"
+#include "BDSP/streams/strategies/byte_stuffing/mixins.h"
+#include "BDSP/streams/strategies/byte_stuffing/types.h"
+
+namespace BDSP::streams::strategies::byte_stuffing {
+
+namespace core {
+class ByteStuffingReadStrategyCore: public abstract::AbstractReadStrategy,
+                             public virtual ByteStuffingConfigsMixin {
+protected:
+    byte_stuffing_config_t _cfg;
+    bool _is_escape_state = false;
+
+    void _init() override;
+    strategies::strategy_read_status_t _send_status_and_reset(uint8_t byte, strategies::strategy_read_status_t status);
+
+public:
+
+    byte_stuffing_config_t get_config();
+    strategies::strategy_read_status_t read(uint8_t byte) override;
+    void reset_read_state() override;
+    set_byte_stuffing_config_status set_config(byte_stuffing_config_t config);
+};
+
+class PPPReadStrategyCore: public ByteStuffingReadStrategyCore, public virtual PPPConfigsMixin {};
+
+} // namespace core
+
+class ByteStuffingReadStrategy final: public core::ByteStuffingReadStrategyCore { };
+class PPPReadStrategy final: public core::PPPReadStrategyCore { };
+
+} // namespace BDSP::streams::strategies::byte_stuffing
+
+#endif // BDSP_STREAMS_STRATEGIES_BYTE_STUFFING_READ_STRATEGY_H
---
+++ src/streams/strategies/byte_stuffing/read.cpp
@@ -0,0 +1,58 @@
+#include "BDSP/streams/PPP/reader.h"
+
+using namespace BDSP::streams::strategies;
+using namespace BDSP::streams::strategies::byte_stuffing;
+using namespace BDSP::streams::strategies::byte_stuffing::core;
+
+
+void ByteStuffingReadStrategyCore::_init() {
+    _cfg = get_default_config();
+}
+
+strategy_read_status_t ByteStuffingReadStrategyCore::_send_status_and_reset(uint8_t byte, strategy_read_status_t status) {
+    _data_callback(byte, status, _context);
+    reset_read_state();
+    return status;
+}
+
+byte_stuffing_config_t ByteStuffingReadStrategyCore::get_config() {
+    return _cfg;
+}
+
+strategy_read_status_t ByteStuffingReadStrategyCore::read(uint8_t byte) {
+    strategy_read_status_t status = STRATEGY_READ_OK;
+    if (byte == _cfg.delimiter_byte) {
+        if (_is_escape_state) {
+            _data_callback(byte, STRATEGY_READ_ERROR, _context);
+        }
+        return _send_status_and_reset(byte, STRATEGY_READ_DELIMITER);
+    }
+    if (_is_escape_state) {
+        _is_escape_state = false;
+        byte ^= _cfg.escape_mask;
+        if (byte not_eq _cfg.escape_byte and byte not_eq _cfg.delimiter_byte) {
+            return _send_status_and_reset(byte, STRATEGY_READ_ERROR);
+        }
+    } else if (byte == _cfg.escape_byte) {
+        _is_escape_state = true;
+        return STRATEGY_READ_OK;
+    }
+    _data_callback(byte, status, _context);
+    return status;
+}
+
+void ByteStuffingReadStrategyCore::reset_read_state() {
+    _is_escape_state = false;
+}
+
+set_byte_stuffing_config_status ByteStuffingReadStrategyCore::set_config(byte_stuffing_config_t config) {
+    set_byte_stuffing_config_status status = _check_config(config);
+    if (_is_escape_state) {
+        status = ERROR_PROCESS_NOT_FINISHED;
+    }
+    if (status == SET_OK) {
+        _cfg = config;
+    }
+    _ready_state_callback(status == SET_OK, _context);
+    return status;
+}
