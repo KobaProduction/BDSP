29441f1ec1496287d3ca848cbfcf367b2537d799
Arthur Koba <caprimores@gmail.com>
2025-02-25 00:18:57 +0300

    update bdsp example

--- examples/Desktop/src/BDSP_EXAMPLE.cpp
+++ examples/Desktop/src/BDSP_EXAMPLE.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #include <vector>
 
-#include <BDSP/streams/COBS/reader.h>
-#include <BDSP/streams/COBS/writer.h>
+#include <BDSP/streams/cobs/reader.h>
+#include <BDSP/streams/cobs/writer.h>
 #include <BDSP/transceiver.h>
 
 #include "utils.h"
 
 using namespace BDSP;
-using namespace BDSP::streams::COBS;
+using namespace BDSP::streams::cobs;
 
 int main() {
-    std::vector<uint8_t> data;
-    for (int i = 1; i < 10; ++i)
-        data.push_back(i);
-    packet_context_t packet_context;
 
-    packet_context.packet_id = 0;
-    packet_context.size = data.size();
-    packet_context.data_ptr = data.data();
 
     BDSPTransceiver transceiver;
 
-    COBSWriter writer;
-    transceiver.set_stream_writer(&writer);
-
-    COBSReader reader;
-    transceiver.set_stream_reader(&reader);
+    COBSZPEWriterStream writer;
+    COBSZPEReaderStream reader;
 
     writer.set_stream_writer(
         [](uint8_t byte, void *ctx) {
-            reinterpret_cast<COBSReader *>(ctx)->read(byte);
+            reinterpret_cast<COBSZPEReaderStream *>(ctx)->read(byte);
         },
         &reader);
 
+
+    transceiver.set_stream_writer(&writer);
+    transceiver.set_stream_reader(&reader);
     transceiver.set_packet_handler(
         [](packet_context_t &packet_ctx, void *ctx) {
             std::cout << "Got packet! Id: " << static_cast<uint32_t>(packet_ctx.packet_id);
@@ -49,7 +42,16 @@ int main() {
         },
         nullptr);
 
-    transceiver.send_packet(packet_context);
+    std::vector<uint8_t> data;
+    for (int i = 1; i < 10; ++i) {
+        data.push_back(i);
+    }
 
+    packet_context_t packet_context;
+    packet_context.packet_id = 0;
+    packet_context.size = data.size();
+    packet_context.data_ptr = data.data();
+
+    transceiver.send_packet(packet_context);
     return 0;
 }
